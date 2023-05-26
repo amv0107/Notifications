0ae5f878910de2d66ff2c77e381dc1ceeed2573f
amv0107 <amv0107@gmail.com>
2023-05-26 11:18:57 +0300

    Task 1: Create a basic notification

--- app/src/main/java/com/amv0107/notifications/MainActivity.kt
+++ app/src/main/java/com/amv0107/notifications/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.amv0107.notifications
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build.VERSION_CODES.N
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+        private const val NOTIFICATION_ID = 0
+    }
+
+    private lateinit var button_notify: Button
+    private lateinit var mNotifyManager: NotificationManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        button_notify = findViewById(R.id.notify)
+
+        button_notify.setOnClickListener {
+            sendNotification()
+        }
+
+    }
+
+
+    private fun createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            // Create a NotificationChannel
+            val notificationChannel =
+                NotificationChannel(PRIMARY_CHANNEL_ID, "Mascot Notification", NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.BLUE
+            notificationChannel.setShowBadge(true)
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Notification from Mascot"
+            mNotifyManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            mNotifyManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun getNotificationBuilder(): NotificationCompat.Builder {
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        return NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+            .setContentTitle("You've been notified!")
+            .setContentText("This is your notification text.")
+            .setSmallIcon(R.drawable.ic_androiod)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+    }
+
+    private fun sendNotification() {
+        val nBuilder = getNotificationBuilder()
+        mNotifyManager.notify(NOTIFICATION_ID, nBuilder.build())
     }
 }
